ee84a1d5a98fff4f066f7a4a0467f0732205718e
aprayadav <51431151+aprayadav@users.noreply.github.com>
2021-12-12 20:10:48 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.assignments.assignment;
+
+public class Main {
+	 public static void main(String[] args) {
+
+	InventoryManager manager = new InventoryManager();
+	manager.inputItemDetails();
+	
+	 }
+}
